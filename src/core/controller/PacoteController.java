a08faf3674699ce8207faccb087ae62fb5bd9ed1
Davi-Avila <davidomingosavila202@gmail.com>
2025-11-03 22:23:46 -0300

    Add files via upload

---
+++ src/core/controller/PacoteController.java
@@ -0,0 +1,70 @@
+package core.controller;
+
+import base.enumeration.Funcionalidade;
+import base.exception.AcomodacaoException;
+import base.exception.PacoteException;
+import base.exception.PessoaException;
+import core.model.Acomodacao;
+import core.model.Funcionario;
+import core.service.PacoteService;
+import core.test.PacoteTest;
+import core.test.PessoaTest;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class PacoteController {
+    private PacoteTest pacoteTest;
+
+    // Construtor
+    public PacoteController(PacoteTest pacoteTest) {
+        this.pacoteTest = pacoteTest;
+    }
+
+    // Gerenciador de testes
+    public void iniciar() throws AcomodacaoException, PacoteException {
+
+        Scanner entrada = new Scanner(System.in);
+        String opcao = null;
+
+        System.out.println(
+                "=== TESTE MODULO PESSOA ===\n"
+                        + "1 - Listar\n"
+                        + "2 - Cadastrar\n"
+                        + "3 - Alterar\n"
+                        + "4 - Excluir"
+        );
+
+        do {
+            System.out.println("\nEscolha a funcionalidade:");
+            opcao = entrada.nextLine();
+
+
+            Funcionalidade funcionalidade = null;
+            switch (opcao) {
+                case "2":
+                    funcionalidade = Funcionalidade.CADASTRAR;
+                    break;
+                case "3":
+                    funcionalidade = funcionalidade.ALTERAR;
+                    break;
+                case "4":
+                    funcionalidade = funcionalidade.EXCLUIR;
+                    break;
+
+            }
+
+            if(funcionalidade != null) {
+                try {
+                    System.out.println("Funcionalidade: " + funcionalidade);
+                    System.out.println(pacoteTest.testar(funcionalidade));
+                } catch(PacoteException excecao) {
+                    System.err.println(excecao.getMessage());
+                }
+            }
+
+        }while (!opcao.isEmpty());
+    }
+
+
+}
---
+++ src/core/test/PacoteTest.java
@@ -0,0 +1,48 @@
+package core.test;
+
+import base.enumeration.Funcionalidade;
+import base.exception.AcomodacaoException;
+import base.exception.PacoteException;
+import base.exception.PessoaException;
+import core.model.Acomodacao;
+import core.service.PacoteService;
+import core.service.PessoaService;
+
+public class PacoteTest {
+    private PacoteService pacoteService;
+
+    // Construtor
+    public PacoteTest(PacoteService pacoteService) {
+        this.pacoteService = pacoteService;
+    }
+    // Métodos de testes
+
+    public String testar(Funcionalidade funcionalidade) throws PacoteException, AcomodacaoException {
+        switch (funcionalidade) {
+            case LISTAR:
+            case CADASTRAR:
+                return this.cadastrar();
+            case EXCLUIR:
+                return this.excluir();
+            default:
+                return null;
+        }
+    }
+
+
+    public String cadastrar() throws PacoteException, AcomodacaoException {
+        // Dados para cadastro
+        String nome = "";
+        String acomodacao = "";
+        String qtdDiarias = "";
+        String valorTotal = "5.0";
+
+        return pacoteService.cadastrar(nome, acomodacao, qtdDiarias, valorTotal);
+    }
+
+    public String excluir() throws PacoteException {
+        // Dados para exclusão
+        String id = "5";
+        return pacoteService.excluir(id);
+    }
+}
